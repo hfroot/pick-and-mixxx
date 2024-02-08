0668761104e21b17f7c452c55130c0b8490e28db
Helen Root <helenroot1@gmail.com>
2024-02-08 17:29:03 +0100

    Remove comment - bug came from using 3.3V not 5V

--- midi_controller/MidiPot.cpp
+++ midi_controller/MidiPot.cpp
@@ -11,7 +11,6 @@ MidiPot::MidiPot(midi::MidiInterface<MIDI_NAMESPACE::SerialMIDI<HardwareSerial> 
 :  _midi_out(midi_out)
 ,  _pin(pin)
 ,  _midi_channel(midi_channel)
-// TODO: investigate whether analog max on the circuit
 ,  _midi_conversion((double)128/1024) // 128 = midi max; 1024 = analog max
 ,  _current_value(0) // 0-127
 ,  _previous_value(0) // 0-127
